a4c6c7e3b824165e9f6aa4c95e119a2c5dd8022f
Charlie Cook <turingbombe@gmail.com>
2016-07-20 19:52:43 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+    def name=(aname)
+        @name = aname
+    end
+    def name
+        @name
+    end
+
+    def job=(ajob)
+        @job = ajob
+    end
+
+    def job
+        @job
+    end
+end
